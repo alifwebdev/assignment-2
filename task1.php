1097a3f94c1c5d6188c6204fa386faa7b4eeeee0
alifwebdev <azizulislam53468@gmail.com>
2026-04-10 23:42:26 +0600

    Assignment-2

---
+++ task1.php
@@ -0,0 +1,15 @@
+<?php
+
+$users = [" rAHim@gmail.com ", " kArim@outlook.com ", " sUmi@yahoo.com "];
+
+foreach ($users as $user) {
+    $user = trim($user);
+
+    $atPos = strpos($user, "@");
+    $rawName = substr($user, 0, $atPos);
+    $domain = substr($user, $atPos + 1);
+
+    $name = ucfirst(strtolower($rawName));
+
+    echo "Name: $name, Domain: $domain\n";
+}
---
+++ task3.php
@@ -0,0 +1,25 @@
+<?php
+
+class Vehicle {
+    protected $brand;
+
+    public function __construct($brand) {
+        $this->brand = $brand;
+    }
+}
+
+class Car extends Vehicle {
+    private $model;
+
+    public function __construct($brand, $model) {
+        parent::__construct($brand);
+        $this->model = $model;
+    }
+
+    public function displayDetails() {
+        echo "This is a $this->brand $this->model.\n";
+    }
+}
+
+$car = new Car("Toyota", "Corolla");
+$car->displayDetails();
